c89fb3d5ca5fcecc4017e312855d39023ca71ac2
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2022-09-21 20:28:28 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'integration-test-v14',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
